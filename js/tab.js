f41484784a79d1cf68f784b3dd39b75117bfe7c6
vlivi <vlyvy97@gmail.com>
2020-02-12 22:44:28 +0700

    First commit
    commit number first

---
+++ js/tab.js
@@ -0,0 +1,8 @@
+function openTab(projectName) {
+  var i;
+  var x = document.getElementsByClassName("tab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(projectName).style.display = "block";
+}
